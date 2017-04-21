ac60a9f29444f4744376e5d1987b9c0c4c225426
Kamil Khan <kamil.z.khan@gmail.com>
2017-04-21 04:50:34 -0700

    prep for cartage

---
+++ Example/Strongbox/Strongbox.h
@@ -0,0 +1,19 @@
+//
+//  Strongbox.h
+//  Strongbox
+//
+//  Created by Kamil Khan on 2017-04-21.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Strongbox.
+FOUNDATION_EXPORT double StrongboxVersionNumber;
+
+//! Project version string for Strongbox.
+FOUNDATION_EXPORT const unsigned char StrongboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Strongbox/PublicHeader.h>
+
+
